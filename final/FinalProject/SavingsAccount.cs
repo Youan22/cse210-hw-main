58befc8d349bf43bb94e2c6ca7ed0beb7ddc8f6a
Youan22 <you18034@byui.edu>
2023-04-05 23:55:33 -0600

    misajours!

--- final/FinalProject/SavingsAccount.cs
+++ final/FinalProject/SavingsAccount.cs
@@ -6,21 +6,21 @@ internal class SavingsAccount
     private string v2;
     private decimal v3;
 
-    public SavingsAccount(string v1, string v2, decimal v3)
+   public SavingsAccount(string accountNumber, string owner, decimal interestRate) : base(accountNumber, owner, interestRate)
     {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
     }
 }// Savings account class
-    public class SavingsAccount : Account
+    public class NewSavingsAccount : Account
     {
         public decimal InterestRate { get; }
 
-        public SavingsAccount(string accountNumber, string owner, decimal interestRate) : base(accountNumber, owner)
-        {
-            InterestRate = interestRate;
-        }
+   public NewSavingsAccount(string accountNumber, string owner, decimal interestRate) : base(accountNumber, owner, 0)
+{
+    InterestRate = interestRate;
+}
 
         public void ApplyInterest()
         {
